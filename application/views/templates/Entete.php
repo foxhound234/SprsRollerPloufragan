30fa016d9d063ca4b6a77e079b7b360ecd9c57a6
foxhound234 <morganlb@protonmail.com>
2018-07-03 11:21:15 +0200

    routing

--- application/views/templates/Entete.php
+++ application/views/templates/Entete.php
@@ -19,55 +19,56 @@
     <div class="navbar-header">
     <?php if(!is_null($this->session->identifiant)) : ?>
     <li class="active navbar-text"><?php echo'Utilisateur connecté : <B>'.$this->session->identifiant.'</B>&nbsp;&nbsp;';?></li>
-    <li class="active  navbar-text"><a href="<?php echo site_url('Supporter/Deconnexion') ?>">Se déconnecter</a>&nbsp;&nbsp;</li>
+    <li class="active  navbar-text"><a href="<?php echo site_url('Deconnexion') ?>">Se déconnecter</a>&nbsp;&nbsp;</li>
     <?php endif;?>
       </a>
      </div>
      <div class="collapse navbar-collapse" id="myNavbar">
      <?php if ($this->session->profil=='A'):?>
      <ul class="nav navbar-nav">
-     <li><a href="<?php echo site_url('Visiteur/Accueil') ?>">Accueil</a></li>
-        <li><a href="<?php echo site_url('Admin/ListedesCommandes') ?>">Commande</a></li>
+     <li><a href="<?php echo site_url('Accueil') ?>">Accueil</a></li>
+        <li><a href="<?php echo site_url('ListedesCommandes') ?>">Commande</a></li>
         <li class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">Ajouter
           <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="<?php echo site_url('Admin/AjouterUnjoueur') ?>">Un Joueur</a></li>
-            <li><a href="<?php echo site_url('Admin/AjouterUneEquipe') ?>">Une Equipe</a></li>
-            <li><a href="<?php echo site_url('Admin/AjouterUnUtilisateur') ?>">Un Utilisateur </a></li>
-            <li><a href="<?php echo site_url('Admin/AjouterUnProduit') ?>">Un Produit </a></li>
-            <li><a href="<?php echo site_url('Admin/AjouterUnSponsor') ?>">Un Sponsor </a></li>
-            <li><a href="<?php echo site_url('Admin/AjouterUneLigue') ?>">Un Ligue </a></li>
-            <li><a href="<?php echo site_url('Admin/AjouterUneTaille') ?>">Une Taille </a></li>
-            <li><a href="<?php echo site_url('Admin/AjouterUnEvenement') ?>">Un Evenement </a></li>
+            <li><a href="<?php echo site_url('AjouterUnjoueur') ?>">Un Joueur</a></li>
+            <li><a href="<?php echo site_url('AjouterUneEquipe') ?>">Une Equipe</a></li>
+            <li><a href="<?php echo site_url('AjouterUnUtilisateur') ?>">Un Utilisateur </a></li>
+            <li><a href="<?php echo site_url('AjouterUnProduit') ?>">Un Produit </a></li>
+            <li><a href="<?php echo site_url('AjouterUnSponsor') ?>">Un Sponsor </a></li>
+            <li><a href="<?php echo site_url('AjouterUneLigue') ?>">Un Ligue </a></li>
+            <li><a href="<?php echo site_url('AjouterUneTaille') ?>">Une Taille </a></li>
+            <li><a href="<?php echo site_url('AjouterUnEvenement') ?>">Un Evenement </a></li>
           </ul>
         </li>
         <li class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">Modifier
           <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="<?php echo site_url('Admin/AfficherLesProduit') ?>">Les Produits</a></li>
-            <li><a href="<?php echo site_url('Admin/ListerLesEquipes') ?>">Les Equipes</a></li>
-            <li><a href="<?php echo site_url('Admin/ListerLesSponsor') ?>">Les Sponsor </a></li>
-            <li><a href="<?php echo site_url('Admin/listerLesjoueursAModifier') ?>">Les Joueurs </a></li>
+            <li><a href="<?php echo site_url('AfficherLesProduit') ?>">Les Produits</a></li>
+            <li><a href="<?php echo site_url('ListerLesEquipes') ?>">Les Equipes</a></li>
+            <li><a href="<?php echo site_url('ListerLesSponsor') ?>">Les Sponsor </a></li>
+            <li><a href="<?php echo site_url('listerLesjoueursAModifier') ?>">Les Joueurs </a></li>
           </ul>
         </li>
         <?php else:?>
 <ul class="nav navbar-nav">
-        <li><a href="<?php echo site_url('Visiteur/Accueil') ?>">Accueil</a></li>
-        <li><a href="<?php echo site_url('Visiteur/Contact')?>">CONTACT</a></li>
-        <li><a href="<?php echo site_url('Visiteur/ListeDesEquipes')?>">LES EQUIPES</a></li>
+        <li><a href="<?php echo site_url('Accueil') ?>">Accueil</a></li>
+        <li><a href="<?php echo site_url('Contact')?>">CONTACT</a></li>
+        <li><a href="<?php echo site_url('ListeDesEquipes')?>">LES EQUIPES</a></li>
         <li class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">CLUB
           <span class="caret"></span></a>
           <ul class="dropdown-menu">
-          <li><a href="<?php echo site_url('Visiteur/Palmares') ?>">Palmares</a></li>
-        <li><a href="<?php echo site_url('Visiteur/Histoire') ?>">Histoire</a></li>
+          <li><a href="<?php echo site_url('Palmares') ?>">Palmares</a></li>
+        <li><a href="<?php echo site_url('Histoire') ?>">Histoire</a></li>
         <li><a href="<?php echo pdf_url('organigramme') ?>">Organigramme</a></li>
         <li><a href="<?php echo pdf_url('CalendrierGeneralRinkHockey20172018') ?>">Plannings et calendriers </a></li>
         <li><a href="<?php echo pdf_url('FICHE_ADHESION_SPRS_2017_2018') ?>">Adhésions </a></li>
         <li><a href="<?php echo pdf_url('Presentation-SPRS-PLOUFRAGAN-saison-2016-2017') ?>">Présentation </a></li>
-        <li><a href="<?php echo site_url('Visiteur/RssEvenement') ?>">Flux rss</a></li>
+        <li><a href="<?php echo site_url('feed') ?>">Flux rss</a></li>
+        <li><a href="https://www.facebook.com/SprsPloufragan/?ref=br_rs">Facebook</a></li>
           </ul>
         </li>
         <li class="dropdown">
@@ -87,21 +88,21 @@
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">Boutique
           <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li> <a href="<?php echo site_url('Visiteur/AfficherLesProduit') ?>">LISTE DES PRODUITS</a></li>
-            <li><a href="<?php echo site_url('Visiteur/AfficherLesCategories')?>">Liste Des Categorie </a></li>
+            <li> <a href="<?php echo site_url('AfficherLesProduitVisiteur') ?>">LISTE DES PRODUITS</a></li>
+            <li><a href="<?php echo site_url('AfficherLesCategories')?>">Liste Des Categorie </a></li>
           </ul>
         </li>
-          <li> <a href="<?php echo site_url('Visiteur/AfficherLePanier') ?>"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
-          <LI><?php echo form_open('Visiteur/RechercheEvenement');?></li>
+          <li> <a href="<?php echo site_url('AfficherLePanier') ?>"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+          <LI><?php echo form_open('RechercheEvenement');?></li>
         <li> <?php  echo form_input(array('name'=>'txtRecherche', 'value'=>'','placeholder'=>'Evenement','pattern'=>'[a-zA-Z\s]+','class'=>'navbar form'));?>
              <li> <?php echo form_submit(array('name'=>'BtnRecherche','value'=>'Recherché','class'=>'navbar form btn btn-primary ','data-icon'=>'search'));?></li>
             <?php echo form_close();?></li>
      <?php endif;?>
      <?php if($this->session->profil==null):?>
-   <li><a href="<?php echo site_url('Visiteur/Connexion') ?>">Connexion</a></li>
-    <li><a href="<?php echo site_url('Visiteur/CreerUnCompte') ?>">Enregistrement</a></li>
+   <li><a href="<?php echo site_url('Connexion') ?>">Connexion</a></li>
+    <li><a href="<?php echo site_url('CreerUnCompte') ?>">Enregistrement</a></li>
     <?php else:?>
-    <li><a href="<?php echo site_url('Supporter/ModifierProfil') ?>">Modifier le Profil</a></li>
+    <li><a href="<?php echo site_url('ModifierProfil') ?>">Modifier le Profil</a></li>
     <?php endif;?>
     </ul>
     </div>
